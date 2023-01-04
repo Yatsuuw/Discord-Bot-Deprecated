cbcd5f637b0181c4640e13f883d9cfaf06130376
Yatsuuw <121401515+Yatsuuw@users.noreply.github.com>
2023-01-04 23:13:33 +0100

    Delete Selects directory

--- Selects/Roles/RolesMenu.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    name: 'roles-menu',
-    async runInteraction(client, interaction) {
-        await interaction.member.roles.add(interaction.values);
-        await interaction.reply({ content: 'Le robot vous a ajouté votre rôle', ephemeral:  true });
-    }
-};
