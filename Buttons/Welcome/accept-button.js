931524013cf09f663037c375bae3312a2404708b
Yatsuuw <121401515+Yatsuuw@users.noreply.github.com>
2023-01-05 18:30:11 +0100

    Update accept-button.js

--- Buttons/Welcome/accept-button.js
+++ Buttons/Welcome/accept-button.js
@@ -3,7 +3,7 @@ module.exports = {
     async runInteraction(client, interaction, member) {
         const fetchGuild = await client.getGuild(interaction.guild);
         await interaction.member.roles.add(fetchGuild.RoleAccept);
-//        await interaction.member.roles.add('978377114673897493');
+        //await interaction.member.roles.add('978377114673897493');
         await interaction.reply({ content: 'Vous avez accepté les règles ! Vous pouvez accéder au serveur.', ephemeral:  true });
     }
 };
