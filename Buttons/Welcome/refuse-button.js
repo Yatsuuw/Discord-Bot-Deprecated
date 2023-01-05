2d3f5a1d2c93a3b6fdfa2e2e13bf7ddd1a5a3bf3
Yatsuuw <121401515+Yatsuuw@users.noreply.github.com>
2023-01-05 18:30:28 +0100

    Update refuse-button.js

--- Buttons/Welcome/refuse-button.js
+++ Buttons/Welcome/refuse-button.js
@@ -3,7 +3,7 @@ module.exports = {
     async runInteraction(client, interaction) {
         const fetchGuild = await client.getGuild(interaction.guild);
         await interaction.member.roles.add(fetchGuild.RoleRefuse);
-//        await interaction.member.roles.add('978377114673897493');
+        //await interaction.member.roles.add('978377114673897493');
 
         try{
             await interaction.member.send(`Tu n'as pas accepté les règles, je t'ai donc expulsé !`);
